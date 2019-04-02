54c9588caa3f812a86545b6fc301be712064ea46
Tracy Lee <tracylee1216@gmail.com>
2019-04-02 18:27:05 +0800

    added new-listing layout

--- app/policies/listing_policy.rb
+++ app/policies/listing_policy.rb
@@ -3,25 +3,24 @@ class ListingPolicy < ApplicationPolicy
     def resolve
       scope.all
     end
+  end
+  # everyone can add a listing
+  def create?
+    true
+  end
 
-    # everyone can add a listing
-    def create?
-      return true
-    end
+  #only the owner can update
+  def update?
+    is_owner_or_admin?
+  end
 
-    #only the owner can update
-    def update?
-      is_owner_or_admin?
-    end
+  def destroy?
+    is_owner_or_admin?
+  end
 
-    def destroy?
-      is_owner_or_admin?
-    end
+  private
 
-    private
-
-    def is_owner_or_admin?
-      record.user == user || user.admin
-    end
+  def is_owner_or_admin?
+    record.user == user || user.admin
   end
 end
