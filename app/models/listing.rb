a0f55ed85b1870339d453bf362551398fd8c21b9
Irina Penzina <irinapenzina@mail.ru>
2019-04-02 11:24:11 +0800

    added validations and relations between models

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,3 +1,11 @@
 class Listing < ApplicationRecord
   belongs_to :user
+  has_many :bookings
+  has_many :reviews, through: :booking
+
+  validates :price, presence: true
+  validates :image, presence: true
+  validates :detail, presence: true
+  validates :drand, presence: true
+  validates :size, presence: true
 end
