fe47f22a8a1e5943b0076c835038b5b9c9903585
Nini Zhu <pn.zhu76@gmail.com>
2019-04-02 14:11:46 +0800

    added instance variable @listings for index method to ListingsController

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,5 +1,6 @@
 class ListingsController < ApplicationController
   def index
+    @listings = Listing.all
   end
 
   def show
@@ -7,16 +8,16 @@ class ListingsController < ApplicationController
 
   def new
   end
-  
+
   def create
   end
 
   def edit
   end
-  
+
   def update
   end
-  
+
   def destroy
   end
 end
