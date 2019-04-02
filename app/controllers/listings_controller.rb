30caf4f958610b115f41a72e23e8e59fcf6a4596
Irina Penzina <irinapenzina@mail.ru>
2019-04-02 18:25:16 +0800

    created show page. added flatpicker for booking part

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -3,20 +3,27 @@ class ListingsController < ApplicationController
   end
 
   def show
+    @listing = Listing.find(params[:id])
   end
 
   def new
   end
-  
+
   def create
   end
 
   def edit
   end
-  
+
   def update
   end
-  
+
   def destroy
+  end
+
+  private
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name, :image)
   end
 end
