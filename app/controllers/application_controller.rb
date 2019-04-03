f1427ac44746f4096d02f239bd7d6ad95d7ca0e2
Nini Zhu <pn.zhu76@gmail.com>
2019-04-03 16:50:43 +0800

    1st review complete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,4 +19,12 @@ class ApplicationController < ActionController::Base
   def skip_pundit?
     devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
   end
+
+  def after_sign_in_path_for(resource)
+    listings_path
+  end
+
+  def after_sign_out_path_for(resource)
+    listings_path
+  end
 end
